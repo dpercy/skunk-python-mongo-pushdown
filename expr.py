005fad61eb397e82739dc42bd94d7204416bd53f
David Percy <david.percy@mongodb.com>
2018-07-16 14:46:27 -0400

    Delete stuff

--- expr.py
+++
@@ -1,8 +0,0 @@
-from collections import namedtuple
-
-Global = namedtuple('Global', 'name')
-Arg = namedtuple('Arg', 'name')
-Call = namedtuple('Call', 'func args')
-GetAttr = namedtuple('GetAttr', 'object name')
-Const = namedtuple('Const', 'value')
-Binop = namedtuple('Binop', 'op left right')
